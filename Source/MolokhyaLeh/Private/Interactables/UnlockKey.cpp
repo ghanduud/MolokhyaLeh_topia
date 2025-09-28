9fd81a50d79430d9e21a8bf5eb42268c73971f19
Omar Salem <omar.ghandy780@gmail.com>
2025-09-29 02:01:27 +0300

    m3lsh commit

--- Source/MolokhyaLeh/Private/Interactables/UnlockKey.cpp
+++ Source/MolokhyaLeh/Private/Interactables/UnlockKey.cpp
@@ -1,27 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "Interactables/UnlockKey.h"
 #include "Inventory/Inventory.h"
 #include "Character/MlCharacter.h"
-#include "Interactables/UnlockKey.h"
 
-// Sets default values
 AUnlockKey::AUnlockKey()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
-
-// Called when the game starts or when spawned
 void AUnlockKey::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AUnlockKey::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
